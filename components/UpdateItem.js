5f3971e74ae0737e69322961e3bde76843b56482
Will Hawks <will@creativedistillery.com>
2018-10-15 14:27:57 -0500

    finished module 4 client side graphql

---
+++ components/UpdateItem.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { Mutation, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+import formatMoney from '../lib/formatMoney';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($ID!) {
+    item(where:  { id: $id }) {
+      id
+      title
+      description
+      price 
+    }
+  }
+`;
+
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    // console.log({ name, type, value });
+    const val = type === 'number' ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    return (
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variable={{
+          id: this.props.id
+        }}
+      >
+        {({ data, loading }) => {
+          if (loading) return <p>Loading....</p>;
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(createItem, { loading, error }) => (
+                <Form
+                  onSubmit={async e => {
+                    // stop form from submitting
+                    e.preventDefault();
+                    // call the mutation
+                    const res = await createItem();
+                    // change them to the single item page
+                    console.log(res);
+                    Router.push({
+                      pathname: '/ item',
+                      query: { id: res.data.createItem.id }
+                    });
+                  }}
+                >
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        type="text"
+                        id="description"
+                        name="description"
+                        placeholder="Enter a description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <button type="submit">Save Changes</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
